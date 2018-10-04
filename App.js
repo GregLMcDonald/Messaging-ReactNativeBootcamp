886aa3eff9a30c6c95fe5cd1835a89d96771e622
Greg McDonald <gregmcdo@gmail.com>
2018-10-04 08:31:38 -0400

    prettified with prettier

--- App.js
+++ App.js
@@ -1,112 +1,128 @@
-import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  Alert, 
-  Image, 
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Image,
   TouchableHighlight,
-  BackHandler,
-} from 'react-native';
+  BackHandler
+} from "react-native";
 
-import Status from './components/Status';
-import Toolbar from './components/Toolbar';
-import ImageGrid from './components/ImageGrid';
+import Status from "./components/Status";
+import Toolbar from "./components/Toolbar";
+import ImageGrid from "./components/ImageGrid";
 
-import MessageList from './components/MessageList';
-import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils';
+import MessageList from "./components/MessageList";
+import {
+  createImageMessage,
+  createLocationMessage,
+  createTextMessage
+} from "./utils/MessageUtils";
 
 export default class App extends React.Component {
-
-
   state = {
     messages: [
-      createTextMessage('Once more unto the breach, dear friends, or fill the wall up with our English dead!'),
-      createTextMessage('hi 1'),
-      createImageMessage('https://unsplash.it/300/300'),
-       createTextMessage('Once more unto the breach, dear friends, or fill the wall up with our English dead!'),
-      createTextMessage('hi 2'),
-      createImageMessage('https://unsplash.it/300/300'),
-       createTextMessage('Once more unto the breach, dear friends, or fill the wall up with our English dead!'),
-      createTextMessage('hi 3'),
-      createImageMessage('https://unsplash.it/300/300'),
-       createTextMessage('Once more unto the breach, dear friends, or fill the wall up with our English dead!'),
-      createTextMessage('hi 4'),
-      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage(
+        "Once more unto the breach, dear friends, or fill the wall up with our English dead!"
+      ),
+      createTextMessage("hi 1"),
+      createImageMessage("https://unsplash.it/300/300"),
+      createTextMessage(
+        "Once more unto the breach, dear friends, or fill the wall up with our English dead!"
+      ),
+      createTextMessage("hi 2"),
+      createImageMessage("https://unsplash.it/300/300"),
+      createTextMessage(
+        "Once more unto the breach, dear friends, or fill the wall up with our English dead!"
+      ),
+      createTextMessage("hi 3"),
+      createImageMessage("https://unsplash.it/300/300"),
+      createTextMessage(
+        "Once more unto the breach, dear friends, or fill the wall up with our English dead!"
+      ),
+      createTextMessage("hi 4"),
+      createImageMessage("https://unsplash.it/300/300")
     ],
 
     fullscreenImageId: null,
-    isInputFocused: false,
-
+    isInputFocused: false
   };
 
   handlePressToolbarCamera = () => {};
   handlePressToolbarLocation = () => {
+    console.log("location pressed");
 
-    console.log( 'location pressed' );
-
-    const { messages } = this.state; 
+    const { messages } = this.state;
 
     //Full signature of getCurrentPosition() API is
     //getCurrentPosition(geo_success, geo_error?, geo_options? )
 
-    navigator.geolocation.getCurrentPosition( (position) => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const {
+          coords: { latitude, longitude }
+        } = position;
 
-      const { coords: { latitude, longitude } } = position;
+        console.log(latitude, longitude);
 
-      console.log( latitude, longitude );
-
-      this.setState({
-       messages: [
-        createLocationMessage({ latitude, longitude, }),
-        ...messages 
-        ],
-      });
-
-    }, () => Alert.alert('Location Error','Please ensure that location services are turned on for this app.') );
+        this.setState({
+          messages: [
+            createLocationMessage({ latitude, longitude }),
+            ...messages
+          ]
+        });
+      },
+      () =>
+        Alert.alert(
+          "Location Error",
+          "Please ensure that location services are turned on for this app."
+        )
+    );
   };
 
-
-  handleChangeFocus = ( isFocused ) => {
-    this.setState( {isInputFocused: isFocused });
-  }
-  handleSubmit = ( text ) => {
+  handleChangeFocus = isFocused => {
+    this.setState({ isInputFocused: isFocused });
+  };
+  handleSubmit = text => {
     const { messages } = this.state;
 
-    this.setState({ 
-      messages: [ createTextMessage(text), ...messages ],
+    this.setState({
+      messages: [createTextMessage(text), ...messages]
     });
   };
 
   dismissFullscreenImage = () => {
-    this.setState( { fullscreenImageId: null } ); 
-  }
+    this.setState({ fullscreenImageId: null });
+  };
 
-  handlePressMessage = ({id,type}) => {
+  handlePressMessage = ({ id, type }) => {
     switch (type) {
-      case 'text':
+      case "text":
         Alert.alert(
-          'Delete message?',
-          'Are you sure you want to delete this for ever and ever?',
+          "Delete message?",
+          "Are you sure you want to delete this for ever and ever?",
           [
-            { 
-              text: 'Cancel',
-              style: 'cancel',
+            {
+              text: "Cancel",
+              style: "cancel"
             },
             {
-              text: 'Delete',
-              style: 'destructive',
+              text: "Delete",
+              style: "destructive",
               onPress: () => {
                 const { messages } = this.state;
-                this.setState( { messages: messages.filter( message => message.id !== id )});
-              },
-            },
-          ],
+                this.setState({
+                  messages: messages.filter(message => message.id !== id)
+                });
+              }
+            }
+          ]
         );
         break;
 
-      case 'image':
-        this.setState( { fullscreenImageId: id, isInputFocused: false, })
+      case "image":
+        this.setState({ fullscreenImageId: id, isInputFocused: false });
         break;
 
       default:
@@ -115,14 +131,13 @@ export default class App extends React.Component {
   };
 
   renderFullscreenImage = () => {
-
     const { messages, fullscreenImageId } = this.state;
 
-    if ( !fullscreenImageId ) return null;
+    if (!fullscreenImageId) return null;
 
-    const image = messages.find( message => message.id === fullscreenImageId );
+    const image = messages.find(message => message.id === fullscreenImageId);
 
-    if ( !image ) return null;
+    if (!image) return null;
 
     const { uri } = image;
     console.log(uri);
@@ -132,53 +147,39 @@ export default class App extends React.Component {
         style={styles.fullscreenOverlay}
         onPress={this.dismissFullscreenImage}
       >
-        <Image style={styles.fullscreenImage} source={{uri}} />
+        <Image style={styles.fullscreenImage} source={{ uri }} />
       </TouchableHighlight>
     );
-  }
+  };
 
-  handlePressImage = ( uri ) => {
+  handlePressImage = uri => {
     const { messages } = this.state;
 
-
-    this.setState({ messages: [ createImageMessage(uri), ...messages ]});
-
-
-
-
-  }
-
+    this.setState({ messages: [createImageMessage(uri), ...messages] });
+  };
 
   renderMessageList = () => {
-
     const { messages } = this.state;
-
-
 
     return (
       <View style={styles.content}>
-        <MessageList 
-          messages = {messages}
-          onPressMessage = {this.handlePressMessage}
+        <MessageList
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
         />
       </View>
-      );
-  }
-
+    );
+  };
 
   renderInputMethodEditor = () => {
-
     return (
       <View style={styles.inputMethodEditor}>
-        <ImageGrid
-          onPressImage={this.handlePressImage}
-        />
+        <ImageGrid onPressImage={this.handlePressImage} />
       </View>
-      );
-  }
+    );
+  };
 
   renderToolbar = () => {
-
     const { isInputFocused } = this.state;
 
     return (
@@ -191,28 +192,28 @@ export default class App extends React.Component {
           onPressLocation={this.handlePressToolbarLocation}
         />
       </View>
-      );
-  }
+    );
+  };
 
   componentWillMount() {
+    this.subscription = BackHandler.addEventListener(
+      "hardwareBackPress",
+      () => {
+        const { fullscreenImageId } = this.state;
 
-    this.subscription = BackHandler.addEventListener( 'hardwareBackPress', () => {
-       const { fullscreenImageId } = this.state;
-
-       if ( fullscreenImageId ) {
+        if (fullscreenImageId) {
           this.dismissFullscreenImage();
           return true;
-       }
+        }
 
-       return false;
-    });
-
+        return false;
+      }
+    );
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.subscription.remove();
   }
-
 
   render() {
     return (
@@ -223,7 +224,6 @@ export default class App extends React.Component {
         {this.renderInputMethodEditor()}
         {this.renderFullscreenImage()}
       </View>
-      
     );
   }
 }
@@ -231,28 +231,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   content: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   inputMethodEditor: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   toolbar: {
     borderTopWidth: 1,
-    borderTopColor: 'rgba(0,0,0,0.1)',
-    backgroundColor: 'white',
+    borderTopColor: "rgba(0,0,0,0.1)",
+    backgroundColor: "white"
   },
   fullscreenOverlay: {
     ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'black',
-    zIndex: 2,
+    backgroundColor: "black",
+    zIndex: 2
   },
   fullscreenImage: {
     flex: 1,
-    resizeMode: 'contain',
-  },
+    resizeMode: "contain"
+  }
 });
